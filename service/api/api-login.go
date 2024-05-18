d5eef73be86357fe267912ecb20abe380c369b70
mcicerani <mcicerani@gmail.com>
2024-05-18 12:27:37 +0200

    first backend files

---
+++ service/api/api-login.go
@@ -0,0 +1,12 @@
+package api
+
+import (
+	"net/http"
+
+	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api/reqcontext"
+	"github.com/julienschmidt/httprouter"
+)
+
+func (rt *_router) doLogin(w http.ResponseWriter, r *http.Request, _ httprouter.Params, ctx reqcontext.RequestContext) {
+
+}
