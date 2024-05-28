1ae50c0de7c32326c34328d6bdb7ab71c17efdc6
mcicerani <mcicerani@gmail.com>
2024-05-28 17:07:08 +0200

    userinfo components

---
+++ webui/src/components/UserListModal.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="user-list-modal">
+        <h2>{{ title }}</h2>
+        <ul>
+            <li v-for="user in userList" :key="user.id">
+                {{ user.username }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        userList: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
