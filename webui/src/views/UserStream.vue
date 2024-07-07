18c8b08be0664f6e6ebd85b4056c872f07e9dd23
mcicerani <mcicerani@gmail.com>
2024-07-07 16:22:57 +0200

    userstream view funzionante

--- webui/src/views/UserStream.vue
+++ webui/src/views/UserStream.vue
@@ -1,14 +1,10 @@
 <script>
-import axios from 'axios'; // Importa l'istanza personalizzata di Axios
-import api from "@/services/axios"
-
-
-
+import api from "@/services/axios";
 
 export default {
     data() {
         return {
-            userStream: [] // Array to store the user stream photos
+            userStream: [] // Initialize userStream as an empty array
         };
     },
     mounted() {
@@ -17,24 +13,49 @@ export default {
     methods: {
         async fetchUserStream() {
             try {
-                const response = await api.get('/api/user/stream'); // Make a GET request to the user stream API endpoint
-                this.userStream = response.data; // Store the fetched user stream photos in the data property
+                const loggedInUserId = localStorage.getItem('loggedInUserId');
+                const response = await api.get(`/users/${loggedInUserId}/stream`, {// Make a GET request to the user stream API endpoint
+                    headers :{
+                        Authorization : localStorage.getItem("token")
+                    }
+                });
+                this.userStream = response.data.Photos; // Store the fetched user stream photos in the data property
             } catch (error) {
                 console.error(error);
+                // Handle error if needed
             }
         }
     }
 };
 </script>
 
+
 <template>
-    <div>
-        <h1>User Stream</h1>
-        <div v-if="userStream.length === 0">No photos in user stream</div>
-        <div v-else>
-            <div v-for="photo in userStream" :key="photo.id">
-                <img :src="photo.url" alt="User Stream Photo">
-            </div>
+    <div class="row mt-4">
+        <div class="col-md-12">
+            <ul class="listaFoto">
+                <li v-for="(photo, index) in userStream" :key="photo.id" :class="{'new-row': index % 1 === 0}">
+                    <div class="card">
+                        <img class="text-center" :src="'data:image/jpeg;base64,' + photo.image_data" alt="User Photo">
+                        <div class="text-center likes">
+                            <p>
+                                <svg class="feather">
+                                    <use href="/feather-sprite-v4.29.0.svg#heart"/>
+                                </svg>
+                                {{ photo.likes }}
+                            </p>
+                        </div>
+                        <div class="text-center comments">
+                            <p>
+                                <svg class="feather">
+                                    <use href="/feather-sprite-v4.29.0.svg#message-square"/>
+                                </svg>
+                                {{ photo.comments }}
+                            </p>
+                        </div>
+                    </div>
+                </li>
+            </ul>
         </div>
     </div>
 </template>
